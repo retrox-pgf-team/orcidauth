c00296c555b0941b203b918e6d824f3581e125c3
Kyle Duffy <44502053+kyduff@users.noreply.github.com>
2022-10-01 12:57:06 +0100

    Change redirect URL to vercel deployment

--- components/implicit-oauth.jsx
+++ components/implicit-oauth.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-const ORCID_URL = "https://orcid.org/oauth/authorize?client_id=APP-6T4JN2RLAHREKHJK&response_type=token&scope=openid&redirect_uri=https://41c0-82-21-152-220.eu.ngrok.io";
+const ORCID_URL = "https://orcid.org/oauth/authorize?client_id=APP-6T4JN2RLAHREKHJK&response_type=token&scope=openid&redirect_uri=https://orcidauth.vercel.app";
 
 export default function OrcidLogin() {
   return (
