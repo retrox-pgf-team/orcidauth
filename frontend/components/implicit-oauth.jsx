136f9491f183d76a4f1ccc7a2a0652ef1be8e394
Kyle Duffy <44502053+kyduff@users.noreply.github.com>
2022-10-02 18:03:44 +0100

    Call contract function

--- frontend/components/implicit-oauth.jsx
+++ frontend/components/implicit-oauth.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const ORCID_URL = "https://orcid.org/oauth/authorize?client_id=APP-6T4JN2RLAHREKHJK&response_type=token&scope=openid&redirect_uri=https://orcidauth.vercel.app";
 const TEST_ORCID_URL = "https://orcid.org/oauth/authorize?client_id=APP-6T4JN2RLAHREKHJK&response_type=token&scope=openid&redirect_uri=https://b190-82-21-152-220.eu.ngrok.io";
 
-const TEST = true;
+const TEST = false;
 
 export default function OrcidLogin() {
   return (
--- frontend/pages/api/issue.js
+++ frontend/pages/api/issue.js
@@ -3,7 +3,7 @@ import { ethers } from 'ethers';
 const network = "maticmum";
 const provider = new ethers.providers.AlchemyProvider(network, process.env.ALCHEMY_API_KEY);
 const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
-const orcidAbi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"_address","type":"address"},{"indexed":true,"internalType":"string","name":"_orcid","type":"string"}],"name":"AddOrcid","type":"event"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"addressToOrcid","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_address","type":"address"},{"internalType":"string","name":"_orcid","type":"string"}],"name":"issue","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"","type":"string"}],"name":"orcidToAddress","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"}]
+const orcidAbi = [{ "inputs": [], "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "_address", "type": "address" }, { "indexed": true, "internalType": "string", "name": "_orcid", "type": "string" }], "name": "AddOrcid", "type": "event" }, { "inputs": [{ "internalType": "address", "name": "", "type": "address" }], "name": "addressToOrcid", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "_address", "type": "address" }, { "internalType": "string", "name": "_orcid", "type": "string" }], "name": "issue", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "string", "name": "", "type": "string" }], "name": "orcidToAddress", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }]
 const orcidContract = new ethers.Contract(process.env.ORCID_CONTRACT, orcidAbi, provider);
 const orcidWithSigner = orcidContract.connect(signer);
 
@@ -18,8 +18,11 @@ async function Issue(req, res) {
   // check for correct data in address and orcid
 
   // operate on address and orcid
-  console.log(`address: ${address}`);
-  console.log(`orcid: ${orcid}`);
+  console.log(`address: ${ address }`);
+  console.log(`orcid: ${ orcid }`);
+
+  const tx = await orcidWithSigner.issue(address, orcid);
+  console.log(tx)
 
   // respond with status
   res.send({
