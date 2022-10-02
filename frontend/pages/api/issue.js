a65482d52b62cfd54d3aae01ce2fe98b07db8cb1
Kyle Duffy <44502053+kyduff@users.noreply.github.com>
2022-10-02 17:56:28 +0100

    Link address to API call

--- frontend/pages/api/issue.js
+++ frontend/pages/api/issue.js
@@ -1,3 +1,12 @@
+import { ethers } from 'ethers';
+
+const network = "maticmum";
+const provider = new ethers.providers.AlchemyProvider(network, process.env.ALCHEMY_API_KEY);
+const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+const orcidAbi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"_address","type":"address"},{"indexed":true,"internalType":"string","name":"_orcid","type":"string"}],"name":"AddOrcid","type":"event"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"addressToOrcid","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_address","type":"address"},{"internalType":"string","name":"_orcid","type":"string"}],"name":"issue","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"","type":"string"}],"name":"orcidToAddress","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"}]
+const orcidContract = new ethers.Contract(process.env.ORCID_CONTRACT, orcidAbi, provider);
+const orcidWithSigner = orcidContract.connect(signer);
+
 async function Issue(req, res) {
 
   // check for erroneous request format
@@ -5,6 +14,8 @@ async function Issue(req, res) {
   // extract content from request
   const body = req.body;
   const { address, orcid } = body;
+
+  // check for correct data in address and orcid
 
   // operate on address and orcid
   console.log(`address: ${address}`);
