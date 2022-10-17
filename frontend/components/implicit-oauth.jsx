751519a727c7e04502c6b7ca2da871611fc937a0
Kyle Duffy <44502053+kyduff@users.noreply.github.com>
2022-10-17 18:53:55 +0100

    Remove 'class' warning

--- frontend/components/implicit-oauth.jsx
+++ frontend/components/implicit-oauth.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function OrcidLogin({ url }) {
   return (
-    <button class="bg-blue-500 hover:bg-blue-700 active:bg-blue-500 text-white font-bold py-2 px-4 rounded">
+    <button className="bg-blue-500 hover:bg-blue-700 active:bg-blue-500 text-white font-bold py-2 px-4 rounded">
       <Link href={url}>Sign In with ORCID</Link>
     </button>
   )
