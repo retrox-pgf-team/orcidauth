b9eff77383a8e902583d9fb0ea8c466479f53136
Kyle Duffy <44502053+kyduff@users.noreply.github.com>
2022-10-17 22:05:57 +0100

    Style the issuances page

--- frontend/pages/issuances.js
+++ frontend/pages/issuances.js
@@ -2,14 +2,23 @@ import GetIssuances from "../utils/getIssuances";
 
 function Index({ issues }) {
   return (
-    <div className="flex flex-col justify-center p-8">
-      {issues.map((item) => (
-        <>
-          <div>{item[3]}</div>
-          <div>{item[2]}</div>
-        </>
-      ))}
-    </div>
+    <>
+      <h1 className="flex justify-center text-5xl p-8">Issuances</h1>
+      <div className="flex flex-col justify-center">
+        {issues.map((item) => (
+          <>
+            <div className="flex flex-row justify-center">
+              <div className="p-2 hover:underline">
+                <a href={`https://orcid.org/${item[3]}`} target="_blank">{item[3]}</a>
+              </div>
+              <div className="p-2 hover:underline">
+                <a href={`https://etherscan.io/address/${item[2]}`} target="_blank">{item[2]}</a>
+              </div>
+            </div>
+          </>
+        ))}
+      </div>
+    </>
   )
 }
 
