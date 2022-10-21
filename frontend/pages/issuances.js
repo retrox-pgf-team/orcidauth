429f9d824468dc58aa8c1112a38da79c703d01b2
Kyle Duffy <44502053+kyduff@users.noreply.github.com>
2022-10-21 13:44:24 +0100

    Add tx link to issuances page

--- frontend/pages/issuances.js
+++ frontend/pages/issuances.js
@@ -5,9 +5,12 @@ function Index({ issues }) {
     <>
       <h1 className="flex justify-center text-5xl p-8">Issuances</h1>
       <div className="flex flex-col justify-center">
-        {issues.map((item) => (
-          <>
-            <div className="flex flex-row justify-center">
+        {issues.reverse().map((item) => (
+          <div className="flex justify-center">
+            <div className="flex flex-row justify-left">
+              <div className="p-2 hover:underline">
+                <a href={`https://mumbai.polygonscan.com/tx/${item[1]}`} target="_blank">{item[0]}</a>
+              </div>
               <div className="p-2 hover:underline">
                 <a href={`https://orcid.org/${item[3]}`} target="_blank">{item[3]}</a>
               </div>
@@ -15,7 +18,7 @@ function Index({ issues }) {
                 <a href={`https://etherscan.io/address/${item[2]}`} target="_blank">{item[2]}</a>
               </div>
             </div>
-          </>
+          </div>
         ))}
       </div>
     </>
