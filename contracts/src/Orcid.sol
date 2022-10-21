ad5ad2ee3c53e83bd226d0810c781f67537cff86
Kyle Duffy <44502053+kyduff@users.noreply.github.com>
2022-10-21 13:17:53 +0100

    Implement and test orcid deletion

--- contracts/src/Orcid.sol
+++ contracts/src/Orcid.sol
@@ -7,6 +7,7 @@ contract Orcid {
   mapping(string => address) public orcidToAddress;
 
   event AddOrcid(address _address, string _orcid);
+  event DeleteOrcid(address _address);
 
   address _owner;
 
@@ -25,6 +26,20 @@ contract Orcid {
 
     // Notify off-chain infrastructure
     emit AddOrcid(_address, _orcid);
+
+  }
+
+  function clear(address _address) external {
+
+    // reject transactions that aren't approved
+    require(msg.sender == _owner || msg.sender == _address, "only authorized parties can delete orcids");
+     
+    string memory orcid = addressToOrcid[_address];
+    orcidToAddress[orcid] = address(0);
+    addressToOrcid[_address] = "";
+
+    // Notify off-chain infrastructure
+    emit DeleteOrcid(_address);
 
   }
 
--- contracts/test/Orcid.js
+++ contracts/test/Orcid.js
@@ -18,7 +18,35 @@ describe("Orcid contract", function () {
   });
 
   it("Should reject transactions from unauthorized parties", async function() {
-    // TODO
-    expect(false).to.equal(true);
+
+    const [owner, addr] = await ethers.getSigners();
+
+    const Orcid = await ethers.getContractFactory("Orcid");
+    const orcid = await Orcid.deploy();
+
+    const x = orcid.connect(addr);
+    expect(x.issue(addr.address, "0000")).to.be.revertedWith("only owner can update orcids");
+    expect(x.clear(addr.address)).to.be.revertedWith("only authorized parties can delete orcids");
+
+  });
+
+  it("Should delete a registered orcid", async function() {
+
+    const [owner, addr] = await ethers.getSigners();
+
+    const Orcid = await ethers.getContractFactory("Orcid");
+    const orcid = await Orcid.deploy();
+
+    const orcid_ = "0000-0000-0000-0000";
+    const address_ = addr.address;
+
+    await orcid.issue(address_, orcid_);
+    expect(await orcid.addressToOrcid(address_)).to.equal(orcid_);
+    expect(await orcid.orcidToAddress(orcid_)).to.equal(address_);
+
+    await orcid.clear(address_);
+    expect(await orcid.addressToOrcid(address_)).to.equal("");
+    expect(await orcid.orcidToAddress(orcid_)).to.equal("0x0000000000000000000000000000000000000000");
+
   })
 })
