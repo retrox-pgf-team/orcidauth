e6e4fb4a5e8fba42aec862c5f77cc81692979e6b
Kyle Duffy <44502053+kyduff@users.noreply.github.com>
2022-10-23 18:07:04 +0100

    Wrap API bodies in try/catch

--- frontend/pages/api/clear.js
+++ frontend/pages/api/clear.js
@@ -5,33 +5,41 @@ const { orcidWithSigner } = getOrcidInfo();
 
 async function Clear(req, res) {
 
-  // check for erroneous request format
-  if (req.method !== 'POST') {
-    return res.status(500).send({ error: "wrong request method" });
+  let errorMsg = "error processing request";
+
+  try {
+
+    // check for erroneous request format
+    if (req.method !== 'POST') {
+      return res.status(500).send({ error: "wrong request method" });
+    }
+
+    console.log("pass method")
+
+    // extract content from request
+    const body = req.body;
+    const { address, signature } = body;
+
+    // operate on address
+    console.log(`address: ${address}`);
+    console.log(`signature: ${signature}`)
+
+    const signed_address = ethers.utils.verifyMessage(address, signature);
+    if (signed_address !== address) {
+      return res.status(500).send({ error: "signature does not match wallet address" });
+    }
+
+    const tx = await orcidWithSigner.clear(address);
+    console.log(tx);
+
+    // Success
+    res.status(200).send({
+      message: "Transaction success"
+    });
+
+  } catch (error) {
+    res.status(500).send({ error: errorMsg });
   }
-
-  console.log("pass method")
-
-  // extract content from request
-  const body = req.body;
-  const { address, signature } = body;
-
-  // operate on address
-  console.log(`address: ${address}`);
-  console.log(`signature: ${signature}`)
-
-  const signed_address = ethers.utils.verifyMessage(address, signature);
-  if (signed_address !== address) {
-    return res.status(500).send({ error: "signature does not match wallet address" });
-  }
-
-  const tx = await orcidWithSigner.clear(address);
-  console.log(tx);
-
-  // Success
-  res.status(200).send({
-    message: "Transaction success"
-  });
 }
 
 export default Clear;
--- frontend/pages/api/issue.js
+++ frontend/pages/api/issue.js
@@ -6,36 +6,44 @@ const { orcidWithSigner } = getOrcidInfo();
 
 async function Issue(req, res) {
 
-  // check for erroneous request format
-  if (req.method !== 'POST') {
-    return res.status(500).send({ error: "wrong request method" });
+  let errorMsg = "error processing request";
+
+  try {
+
+    // check for erroneous request format
+    if (req.method !== 'POST') {
+      return res.status(500).send({ error: "wrong request method" });
+    }
+
+    console.log("pass method")
+
+    // extract content from request
+    const body = req.body;
+    const { address, orcid, signature } = body;
+
+    // check for correct data in address and orcid
+
+    // operate on address and orcid
+    console.log(`address: ${address}`);
+    console.log(`orcid: ${orcid}`);
+    console.log(`signature: ${signature}`)
+
+    const signed_address = ethers.utils.verifyMessage(orcid, signature);
+    if (signed_address !== address) {
+      return res.status(500).send({ error: "signature does not match wallet address" });
+    }
+
+    const tx = await orcidWithSigner.issue(address, orcid);
+    console.log(tx);
+
+    // respond with status
+    res.status(200).send({
+      message: "Transaction success"
+    });
+
+  } catch (error) {
+    res.status(500).send({ error: errorMsg });
   }
-
-  console.log("pass method")
-
-  // extract content from request
-  const body = req.body;
-  const { address, orcid, signature } = body;
-
-  // check for correct data in address and orcid
-
-  // operate on address and orcid
-  console.log(`address: ${address}`);
-  console.log(`orcid: ${orcid}`);
-  console.log(`signature: ${signature}`)
-
-  const signed_address = ethers.utils.verifyMessage(orcid, signature);
-  if (signed_address !== address) {
-    return res.status(500).send({ error: "signature does not match wallet address" });
-  }
-
-  const tx = await orcidWithSigner.issue(address, orcid);
-  console.log(tx);
-
-  // respond with status
-  res.status(200).send({
-    message: "Transaction success"
-  });
 }
 
 export default Issue;
