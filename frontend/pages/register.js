56083a32c7ced47d8579baf0500c832e62b413b1
Fernando Almansa <fernando@FerMBP.local>
2022-11-02 20:14:37 +0000

    Display full address and in larger font

--- frontend/pages/register.js
+++ frontend/pages/register.js
@@ -76,7 +76,7 @@ export default function Home({ orcid_url }) {
           </h4>
           <div className="mx-auto my-2 mb-4">
             { address ?
-            <h4 className="text-center text-lg">{address.slice(0, 5) + "..." + address.slice(-4)}</h4> :
+            <h4 className="text-center text-xl">{address}</h4> : // {address.slice(0, 5) + "..." + address.slice(-4)}
             <ConnectButton />}
           </div>
         </div>
