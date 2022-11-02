37029b02902a1ab5552aeb8a27e8c7cad5a343ac
Kyle Duffy <44502053+kyduff@users.noreply.github.com>
2022-11-02 19:53:03 +0000

    Add nav bar to issuances

--- frontend/pages/issuances.js
+++ frontend/pages/issuances.js
@@ -1,26 +1,34 @@
+import { Navbar } from '../components/layout';
 import GetIssuances from "../utils/getIssuances";
 
 function Index({ issues }) {
   return (
     <>
-      <h1 className="flex justify-center text-5xl p-8">Issuances</h1>
-      <div className="flex flex-col justify-center">
-        {Object.values(issues).reverse().map((item) => (
-          <div className="flex justify-center">
-            <div className="flex flex-row justify-left">
-              <div className="p-2 hover:underline">
-                <a href={`https://mumbai.polygonscan.com/tx/${item.txn}`} target="_blank">View Transaction</a>
-              </div>
-              <div className="p-2 hover:underline">
-                <a href={`https://orcid.org/${item.orcid}`} target="_blank">{item.orcid}</a>
-              </div>
-              <div className="p-2 hover:underline">
-                <a href={`https://etherscan.io/address/${item.address}`} target="_blank">{item.address}</a>
+      <Navbar />
+      <section className='mt-20'>
+        <div>
+          <h2 className='text-center text-4xl font-bold'>
+            Issuances
+          </h2>
+        </div>
+        <div className="flex flex-col justify-center">
+          {Object.values(issues).reverse().map((item) => (
+            <div className="flex justify-center">
+              <div className="flex flex-row justify-left">
+                <div className="p-2 hover:underline">
+                  <a href={`https://mumbai.polygonscan.com/tx/${item.txn}`} target="_blank">View Transaction</a>
+                </div>
+                <div className="p-2 hover:underline">
+                  <a href={`https://orcid.org/${item.orcid}`} target="_blank">{item.orcid}</a>
+                </div>
+                <div className="p-2 hover:underline">
+                  <a href={`https://etherscan.io/address/${item.address}`} target="_blank">{item.address}</a>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      </section>
     </>
   )
 }
