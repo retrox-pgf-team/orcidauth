7862b2e721c4b233c5ea92e0f8a2298434925a1c
Kyle Duffy <44502053+kyduff@users.noreply.github.com>
2022-11-02 20:37:48 +0000

    Hydrate profile page with ORCID (if exists)

--- frontend/pages/profile.js
+++ frontend/pages/profile.js
@@ -1,6 +1,39 @@
+import { ethers } from 'ethers';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import { useAccount } from 'wagmi';
 import { Navbar } from '../components/layout';
+import { orcidAbi } from '../utils/orcidAbi';
 
-export default function Home() {
+export default function Home({ contractAddress }) {
+
+  const { address } = useAccount();
+  const [orcid, setOrcid] = useState('');
+
+
+  useEffect(() => {
+
+    const provider = new ethers.providers.JsonRpcProvider(
+      'https://polygon-mumbai.g.alchemy.com/v2/Lvg6Sp2FS9yUK_PFYFvK2fSY87w3Iftg'
+    );
+    const contract = new ethers.Contract(contractAddress, orcidAbi, provider);
+
+    const getOrcid = async () => {
+
+      if (!address) {
+        return '';
+      }
+
+      const txn = await contract.addressToOrcid(address);
+      return txn
+
+    }
+
+    getOrcid().then((value) => {
+      setOrcid(value)
+    })
+
+  }, [])
 
   return (
     <>
@@ -15,8 +48,23 @@ export default function Home() {
           <h4 className='text-center text-2xl mt-4'>
             Associate your academic credentials with a wallet address
           </h4>
+          <h2 className='text-center text-2xl mt-4'>
+            {orcid ||
+              <div>
+                <Link href={"/register"}>Register your ORCID</Link>
+              </div>
+            }
+          </h2>
         </div>
       </section>
     </>
   )
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      contractAddress: process.env.ORCID_CONTRACT,
+    }
+  }
 }
